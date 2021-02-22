59bb832bfb8a063c8c52aa15a781eb038d668c7a
geekymayur <rajmayur219@gmail.com>
2021-02-22 22:11:25 +0530

    dice working with all added features

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var showDiceFace = document.getElementById("showDiceFace");
+function rollme() {
+    showDiceFace.src = `/assets/dice2.gif`
+
+
+    // showDiceFace.src = `/assets/dice-face/diceNum${DiceNum}.png`
+    function rollDice() {
+        var DiceNum = Math.floor(Math.random() * 6) + 1;
+
+        showDiceFace.src = `/assets/dice-face/diceNum${DiceNum}.png`
+        console.log(DiceNum);
+    }
+    setTimeout(rollDice, 2000)
+    // setTimeout(rollDice);
+}
